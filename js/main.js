063fbcefa6ee73003495528f401ddd0699f36701
Maryam Ali <mali62541@gmail.com>
2015-11-04 14:59:35 -0500

    headers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+import React from 'react';
+import ReactDom from 'react-dom';
+import Router from './router';
+import './ajaxSetup';
+import Cookies from 'js-cookie';
+
+
+let element = document.querySelector('.app');
+var router= new Router(element);
+router.start();
+
+window.router=router;
 
 console.log('Hello, World');
---
+++ js/router.js
@@ -0,0 +1,24 @@
+import Backbone from 'backbone';
+import React from 'react';
+import ReactDom from 'react-dom';
+import $ from 'jquery';
+import Cookies from 'js-cookie';
+
+
+export default Backbone.Router.extend({
+
+
+
+  routes: {
+        
+
+  },
+
+  
+
+  start: function (){
+    Backbone.history.start();
+  }
+});
+
+export default Router;
