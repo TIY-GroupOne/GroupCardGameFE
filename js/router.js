c9d0756e5e8151d712ad7fcea9054769ee8dabae
Maryam Ali <mali62541@gmail.com>
2015-11-05 10:08:13 -0500

    more router stuff

--- js/router.js
+++ js/router.js
@@ -13,9 +13,14 @@ export default Backbone.Router.extend({
 
   routes: {
    // first view has log-in, sign-up and rules
-    ""        : "homeView",
-    "game"    : "gameView",
-    "signOut" : "signOutView"
+    ""             : "homeView",
+    "game"         : "gameView",
+    "chooseDeck"   : "chooseADeckView",
+    "currentDecks" : "currentDecksView",
+    "createDeck"   : "createANewDeckView",
+    "mainGame"     : "mainGameView",
+    "signOut"      : "signOutView",
+    "goodBye"      : "goodByeView"
 
   },
 
@@ -40,28 +45,93 @@ render (component) {
     this.render(
           // tab container goes here
           //log-in + sign-in buttons go here
+          //'About Game' goes here
+          //"START" button goes here, directs to GameView below
         );
       });
   },
 
-    showGameView() {
+  showGameView() {
     this.render(
-      //card-containing divs go here
+
+      
+      <GameComponent   
+      onChooseADeckClick={() => this.goto('chooseADeckView')}
+      onCreateANewDeckClick={() => this.goto('createANewDeckView')}/> 
+    );
+  },
+
+  showChooseADeck() {
+    this.render(
+      
+      //FIXME
+      // preset card-containing divs go here
+      // AFTER deck is choosen, return to main game view
+      <ChooseComponent
+      onCreateANewDeckClick={() => this.goto('createANewDeckView')}/> 
+    );
+  },  
+
+  showCurrentDecks() {
+    this.render(
+      // preset card-containing divs go here
+      
+      //FIXME
+      //needs a button to return to 
+      <CurrentComponent
+      onPlayClick={( => this.goto('mainGameView'))}   
+      onCreateANewDeckClick={() => this.goto('createANewDeckView')}/> 
+    );
+  },  
+
+  showCreateANewDeckView() {
+    this.render(
+      <CreateComponent  
+      onDeckCompleteClick={() => this.goto ('mainGameView')}/> 
+    );
+  },
+
+  showMainGameView() {
+    this.render(
+      // choosen card deck goes here
+      
+      <MainGameComponent
+      // FIXME 
+      // fill in Choose a card deck route     
+      onSignOutClick={() => this.goto('goodByeView')}/> 
+    );
+  }, 
+  
+  showSignOutView() {
+    this.render(
+      // FIXME
+      // sign out/delete profile buttons go here
+      <SignOutComponent
+      onReturnClick={() => this.goto('mainGameView')}
+      onSignOutClick={() => this.goto('goodbyeView')}   
+      onDeleteClick={() => this.goto('goodyeView')}/>
       
     );
   },
 
-    showSignOutView() {
+  showGoodbyeView() {
     this.render(
-      // sign out/delete profile buttons go here
+      // choosen card deck goes here
+      
+      <GoodByeComponent
+      // FIXME 
+      // text for goodbye page 
+      // do we want a return to game page    
+      onClick={() => this.goto('')}/> 
     );
+  },  
 
   showSpinner () {
     ReactDom.render(<SpinnerComponent/>);
   },
   
 
-  start: function (){
+start: function (){
     Backbone.history.start();
   }
 });
