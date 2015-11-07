4deae1e1225925eaea19ff5df7d5bc7f469f6da1
Maryam Ali <mali62541@gmail.com>
2015-11-07 12:33:09 -0500

    cards rendering demo

--- js/views/mainGame.js
+++ js/views/mainGame.js
@@ -21,6 +21,62 @@ export default React.createClass({
       </div>
 
       );
+
+    //REACT PART
+
+    //app part, 
+    import React from 'react';
+
+
+    var App = React.createClass({
+        getInitialState: function() {
+            return {
+                flipped: false
+            };
+        },
+
+        flip: function() {
+            this.setState({ flipped: !this.state.flipped });
+        },
+
+        render: function() {
+            return <div>
+                <Flipper flipped={this.state.flipped} orientation="horizontal" />
+                <Flipper flipped={this.state.flipped} orientation="vertical" />
+
+                <div className="button-container">
+                    <button onClick={this.flip}>Flip!</button>
+                </div>
+            </div>;
+        }
+    });
+
+
+    //Flipper part
+
+    var Flipper = React.createClass({
+        render: function() {
+            return <div className={"flipper-container " + this.props.orientation}>
+                <div className={"flipper" + (this.props.flipped ? " flipped" : "")}>
+                    <Front>the front!</Front>
+                    <Back>the back!</Back>
+                </div>
+            </div>;
+        }
+    });
+
+    //Flipper part 2: front, back
+    var Front = React.createClass({
+        render: function() {
+            return <div className="front tile">{this.props.children}</div>;
+        }
+    });
+
+    var Back = React.createClass({
+        render: function() {
+            return <div className="back tile">{this.props.children}</div>;
+        }
+    });
        console.log('main game view'); 
   }
 });
