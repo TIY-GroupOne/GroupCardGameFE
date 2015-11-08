d130b0c68c68e0d98d2928e641d5ce8f7b37e4bf
Jontae Pierce <jontae.pierce@gmail.com>
2015-11-08 18:09:48 -0500

    Trying new material UI JSX

--- js/main.js
+++ js/main.js
@@ -4,6 +4,32 @@ import Router from './router';
 import './ajaxSetup';
 import Cookies from 'js-cookie';
 import $ from 'jquery';
+let injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+const AppBar = require('material-ui/lib/app-bar');
+const RaisedButton = require('material-ui/lib/raised-button');
+
+const ThemeManager = require('material-ui/lib/styles/theme-manager');
+const ThemeDecorator = require('material-ui/lib/styles/theme-decorator');
+import IconButton from 'material-ui/lib/icon-button';
+import MenuItem from 'material-ui/lib/menu/menu-item';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import NavigationClose from 'material-ui/lib/'
+
+ReactDom.render(
+  <AppBar
+    title="Cardyo"
+  />,
+  document.querySelector('.topbar')
+);
+
 
 
 new Router().start();
